258b16672eba70f5732c499adaa5a6d74ea347f4
Alvaro Garcia <alvarogarciainfo@gmail.com>
2023-06-22 10:46:42 -0300

    lambda code

--- AWS/aws-node-http-api-project/index.js
+++ AWS/aws-node-http-api-project/index.js
@@ -1,13 +1,35 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const Redis = require('ioredis');
+const redis = new Redis({
+  host: '<RedisEndpoint>',
+  port: 6379,
+  password: '<RedisPassword>',
+});
+
+exports.handler = async (event) => {
+  const key = 'miClave';
+  
+  try {
+    // Verificar si el valor existe en Redis
+    const existingValue = await redis.get(key);
+    
+    if (existingValue) {
+      // El valor existe en Redis, realizar la acción necesaria
+      console.log('El valor existe en Redis:', existingValue);
+      // Realizar la acción necesaria con el valor existente
+      return 'Operación exitosa';
+    } else {
+      // El valor no existe en Redis, guardarlo
+      const newValue = 'miNuevoValor';
+      await redis.set(key, newValue);
+      console.log('El valor ha sido guardado en Redis:', newValue);
+      // Realizar la acción necesaria con el valor guardado
+      return 'Operación exitosa';
+    }
+  } catch (error) {
+    console.error('Error al realizar la operación en Redis:', error);
+    throw error;
+  } finally {
+    // Cerrar la conexión a Redis después de cada ejecución
+    redis.quit();
+  }
 };
